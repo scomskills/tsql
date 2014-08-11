9c0e7b297556f1357e7a167bead600c879a2c144
Jonathan Almquist <jonathan@scomskills.com>
2014-08-11 10:12:59 -0500

    Updated DwMonitorToInstances

--- DwMonitorToInstances.sql
+++ DwMonitorToInstances.sql
@@ -7,9 +7,9 @@ USE OperationsManagerDW
 DECLARE @MonitorName AS NVARCHAR(MAX)
 SET @MonitorName = 'Logical Disk Free Space'
 SELECT vmon.MonitorDefaultName AS 'Monitor', vmet.ManagedEntityTypeDefaultName AS 'Monitor Target', vme.Path, vme.DisplayName AS 'Instance Name'
-FROM vmon AS vmon INNER JOIN
-    vmonManagementPackVersion AS vmmpv ON vmon.MonitorRowId = vmmpv.MonitorRowId INNER JOIN
-    vTypedManagedEntity AS vtme ON vmmpv.TargetManagedEntityTypeRowId = vtme.ManagedEntityTypeRowId INNER JOIN
-    vManagedEntity AS vme ON vtme.ManagedEntityRowId = vme.ManagedEntityRowId INNER JOIN
-    vManagedEntityType AS vmet ON vtme.ManagedEntityTypeRowId = vmet.ManagedEntityTypeRowId
+FROM vMonitor AS vmon INNER JOIN
+	vMonitorManagementPackVersion AS vmmpv ON vmon.MonitorRowId = vmmpv.MonitorRowId INNER JOIN
+	vTypedManagedEntity AS vtme ON vmmpv.TargetManagedEntityTypeRowId = vtme.ManagedEntityTypeRowId INNER JOIN
+	vManagedEntity AS vme ON vtme.ManagedEntityRowId = vme.ManagedEntityRowId INNER JOIN
+	vManagedEntityType AS vmet ON vtme.ManagedEntityTypeRowId = vmet.ManagedEntityTypeRowId
 WHERE vmon.MonitorDefaultName = @MonitorName
